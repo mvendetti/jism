0d557721e189b3f4da8537407823d3f0cdffc7ff
Lee Hilton <lee.hilton@hipstercreative.com>
2017-02-09 19:58:37 -0800

    Mac mutator is now better at it’s job

--- app/Traits/EnforceMacFormatTrait.php
+++ app/Traits/EnforceMacFormatTrait.php
@@ -6,7 +6,8 @@ trait EnforceMacFormatTrait
 {
     public function setMacAttribute($value)
     {
-        $value = preg_split( "( |\:|\-)", $value );
+        $value = str_replace([' ', '-', ':', '_'], '', $value);
+        $value = str_split($value, 2);
         $value = implode(":", array_filter($value));
         $this->attributes['mac'] = strtoupper($value);
     }
