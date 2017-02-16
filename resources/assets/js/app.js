ea0194e96cbb84cfa8c82de1237c53e116a5cabd
mike <mike@hipstercreative.com>
2017-02-16 14:44:33 -0800

    Multiple footers
    also better folder structure

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -16,14 +16,26 @@ require('./bootstrap');
 const LayoutPrimary = require('./layouts/primary.vue')
 const PageHome = require('./pages/home.vue')
 const Header = require('./components/header.vue')
-const Footer = require('./components/footer.vue')
-const Camera = require('./components/camera.vue')
-const Pod = require('./components/pod.vue')
+const Footer = require('./components/footers/footer.vue')
+const FooterStandard = require('./components/footers/footerStandard')
+const FooterMain = require('./components/footers/footerMain.vue')
+const FooterMainSettings = require('./components/footers/footerMainSettings.vue')
+const FooterAvailable = require('./components/footers/footerAvailable')
+const FooterPod = require('./components/footers/footerPod.vue')
+const FooterCamera = require('./components/footers/footerCamera.vue')
+const Camera = require('./pages/camera.vue')
+const Pod = require('./pages/pod.vue')
 
 Vue.component('jism-layout-primary', LayoutPrimary);
 Vue.component('jism-page-home', PageHome);
 Vue.component('jism-header', Header);
 Vue.component('jism-footer', Footer);
+Vue.component('jism-footer-standard', FooterStandard);
+Vue.component('jism-footer-main', FooterMain);
+Vue.component('jism-footer-main-settings', FooterMainSettings);
+Vue.component('jism-footer-available', FooterAvailable);
+Vue.component('jism-footer-pod', FooterPod);
+Vue.component('jism-footer-camera', FooterCamera);
 Vue.component('jism-camera', Camera);
 Vue.component('jism-pod', Pod);
 
