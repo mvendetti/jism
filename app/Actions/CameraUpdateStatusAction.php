00d15f9117bf884b19f5213a5b5d4174fed0ab42
Lee Hilton <lee.hilton@hipstercreative.com>
2017-02-17 14:54:30 -0800

    Camera can now be failed

--- app/Actions/CameraUpdateStatusAction.php
+++ app/Actions/CameraUpdateStatusAction.php
@@ -15,6 +15,12 @@ class UpdateStatusAction
             $camera = $cameras->where('ip', $key)->first();
             $status = CameraStatus::create(['camera_id' => $camera->serial_number, 'raw' => $value]);
         }
+        foreach($api->getFailed() as $key => $value)
+        {
+            $camera = $cameras->where('ip', $key)->first();
+            $camera->online = false;
+            $camera->save();
+        }
     }
 
     static public function run($cameras)
