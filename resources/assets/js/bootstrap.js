e31c165b206c554b5b5a8e46f108bc72df3fbd1e
mike <mike@hipstercreative.com>
2017-03-20 11:16:11 -0700

    Moment

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -7,6 +7,8 @@ window._ = require('lodash');
  * code may be modified to fit the specific needs of your application.
  */
 
+window.moment = require('moment');
+
 window.$ = window.jQuery = require('jquery');
 
 require('bootstrap-sass');
