625e9f12ce4474176b541ac0ad2f22f84d7f2ed1
mike <mike@hipstercreative.com>
2017-03-31 14:48:26 -0700

    Pod model stuff

--- app/Pod.php
+++ app/Pod.php
@@ -18,8 +18,16 @@ class Pod extends Model
     /**
      * The roles that belong to the user.
      */
-    public function cameras()
+    public function camera_left()
     {
-        return $this->hasMany('App\Camera');
+        return $this->belongsTo('App\Camera', 'camera_left_id');
+    }
+
+    /**
+     * The roles that belong to the user.
+     */
+    public function camera_right()
+    {
+        return $this->belongsTo('App\Camera', 'camera_right_id');
     }
 }
