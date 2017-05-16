f0fd07d580656fa9d924870a0cf963c0347d508e
mike <mike@hipstercreative.com>
2017-05-16 14:41:46 -0700

    Push home instead

--- resources/assets/js/components/footer/footerAdd.vue
+++ resources/assets/js/components/footer/footerAdd.vue
@@ -3,6 +3,6 @@
         <jism-button-stop></jism-button-stop>
         <jism-button-record></jism-button-record>
         <router-link :to="{ name: 'remove' }"><i class="fa fa-minus" aria-hidden="true"></i></router-link>
-        <router-link :to="{ name: 'settings' }"><i class="fa fa-arrow-left" aria-hidden="true"></i></router-link>
+        <router-link :to="{ name: 'home' }"><i class="fa fa-arrow-left" aria-hidden="true"></i></router-link>
     </div>
 </template>
