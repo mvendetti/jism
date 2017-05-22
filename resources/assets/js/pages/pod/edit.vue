4d7d94bb7ad12e7a86ca089ffd2a648e3eda634b
mike <mike@hipstercreative.com>
2017-05-22 14:51:11 -0700

    Disable per pod screen

--- resources/assets/js/pages/pod/edit.vue
+++ resources/assets/js/pages/pod/edit.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <jism-layout-primary>
-            <h1>Pod Settings</h1>
+            <h1>Pod {{ pod.number }} Settings</h1>
             <div class="row">
                 <div class="col-md-12 col-xs-12">
                     <button @click="sleep" class="btn btn-default">Sleep</button>
@@ -9,13 +9,12 @@
                 </div>
             </div>
 
-            <h3>Disable Pods</h3>
+            <h3>Disable</h3>
             <ul class="list-group">
-                <li v-for="pod in pods" :class="['list-group-item', pod.disabled ? 'disabled' : '']">
+                <li :class="['list-group-item', pod.disabled ? 'disabled' : '']">
                     <span>P{{ pod.number }}</span>
-                    <input @click="pod.disabled = !pod.disabled" @change="disable(pod.id, pod.disabled)" :value="'pod' + pod.number" type="checkbox" :checked="pod.disabled" class="pull-right">
+                    <input @click="pod.disabled = !pod.disabled" @change="disable(pod.disabled)" :value="'pod' + pod.number" type="checkbox" :checked="pod.disabled" class="pull-right">
                 </li>
-                <li v-if="!pods.length" class="list-group-item">No pods to disable</li>
             </ul>
         </jism-layout-primary>
     </div>
@@ -24,18 +23,16 @@
 <script>
     import { mapGetters } from 'vuex'
     export default {
-        computed: mapGetters('landlord', ['pods']),
+        computed: mapGetters('landlord', ['pod']),
         methods: {
-            disable(id, state) {
-                var that = this,
-                    data = { 'disabled': state, id };
+            disable(state) {
+                var data = { 'disabled': state };
 
-                this.$store.dispatch('pod/UPDATE', data)
-                    .then(function() {
-                        if(form.successful) {
-                            that.$router.push({ name: 'pod-settings' });
-                        }
-                    });
+                axios.patch('/api/pod/' + this.pod.number, data).then((response) => {
+                   console.log(response.data);
+               }, (error) => {
+                   console.log(error.response.data);
+               });
             },
             sleep() {
                 axios.post('/api/pod/' + this.podId + '/sleep').then((response) => {
