1862810479c4f1b28f741431c112d8cfe7941a4d
mike <mike@hipstercreative.com>
2017-06-06 11:04:09 -0700

    Fix pod index heading

--- resources/assets/js/pages/pod/index.vue
+++ resources/assets/js/pages/pod/index.vue
@@ -1,13 +1,14 @@
 <template>
     <div>
         <jism-layout-primary>
-            <h1>Pod {{ $route.params.pod_number }}</h1>
+            <h1>Pod {{ pod.number }}</h1>
         </jism-layout-primary>
     </div>
 </template>
 
 <script>
+    import { mapGetters } from 'vuex'
     export default {
-        //
+        computed: mapGetters('landlord', ['pod']),
     }
 </script>
